6a6e84a367e69bac6db9c6fa8523108f0344a25d
Tunjay Akbarli <128295620+Tuncayscript@users.noreply.github.com>
2024-10-05 19:54:14 +0400

    Update __init__.py

--- Lib/base64/__init__.py
+++ Lib/base64/__init__.py
@@ -1,3 +1,14 @@
+# Copyright (c) 2024, NeXTHub Corporation. All rights reserved.
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+#
+# This code is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# version 2 for more details (a copy is included in the LICENSE file that
+# accompanied this code).
+
+# Created by Tunjay Akbarli on October 5th, 2024.
+
 """Implements the base64 package."""
 
 from .base64 import b16decode, b16encode, b64decode, b64encode
